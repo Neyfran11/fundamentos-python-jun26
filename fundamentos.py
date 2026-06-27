5668d0c757e52d1c27a7ded00ebed314ce1d1c84
Neyfran11 <larrsswild@gmail.com>
2026-06-27 11:39:46 -0400

    ciclo for y estrucuturas de datos

--- fundamentos.py
+++ fundamentos.py
@@ -90,20 +90,94 @@ div = num1 / num2
 
 
 
-player1 = "Messi"
+#player1 = "Messi"
 
-player2 = "Ronaldo"
+#player2 = "Ronaldo"
 
-sel_player1 = "piedra"
-sel_player2 = "tijeras"
+#sel_player1 = "piedra"
+#sel_player2 = "tijeras"
 
-if sel_player1 == "piedra":
-    print(f"{player1} piedra para tijeras")
-    print(f"{player1} tijeras para papel")
-    print(f"{player1} papel para piedra")
+#if sel_player1 == "piedra":
+   # print(f"{player1} piedra para tijeras")
+   # print(f"{player1} tijeras para papel")
+  #  print(f"{player1} papel para piedra")
 
-elif sel_player2 == "Ronaldo":
-    print(f"{player2} piedra para tijeras")
-    print(f"{player2} tijeras para papel")
-    print(f"{player2} papel para piedra")
+#elif sel_player2 == "Ronaldo":
+    #print(f"{player2} piedra para tijeras")
+   # print(f"{player2} tijeras para papel")
+   # print(f"{player2} papel para piedra")
+
+
+
+# Estrcuturas de datos
+name_list = ["Alfredo", "Primero", "Segundo", "Tercero"]
+age_list = [18, 20, 30, 40]
+weights_list = [50.5, 60.5, 70.5, 80.5]
+
+name_list2 = [
+              "Alfredo"
+              "Primero"
+              "Segundo"
+              "Tercero"
+]
+
+
+# agregando elementos a la lista
+#name_list.append("Cuarto")
+
+#print(len(name_list))
+
+#print(name_list)
+
+#print(name_list[2]) # Segundo
+#print(name_list[0]) # Alfredo
+#print(name_list[1]) # Primero
+#print(name_list[3]) # Tercero
+
+# mostrar un slice de la lista 
+#print(name_list[1:3]) # ['Alfredo', 'Primero']
+
+#el ultimo elemento de la lista
+#print(name_list[-1]) # Tercero
+
+# cambiar el valor del primer elemento de la lista
+#name_list[0] = "ruperto" 
+#print(name_list) # ['ruperto', 'Primero', 'Segundo', 'Tercero']
+
+# tuplas
+#name_tuple = (
+  #  "Alfredo", 
+   # "Primero", 
+    #"Segundo", 
+    #"Tercero")
+
+#print(name_tuple[0]) # Alfredo
+
+# diccionario 
+
+name_dict = {
+    "name": "Alfredo",
+    "last_name": "Primero",
+    "age": 20,
+    "weight": 70.5,
+    "favorite_foods": ["Pizza", "Hamburguesa", "Tacos"],
+    "address": {
+        "zip_code": "12345",
+        "street": "Calle 123",
+        "city": "lecheria",
+        "country": "Pais",
+    }
+}
+
+#print(name_dict["address"]["city"]) # ciudad
+#print(name_dict["favorite_foods"][1]) # Hamburguesa
+
+# bucles
+#for name in name_list:
+ #   if name == "Segundo":
+ #       print(f"hola {name}")
+
+len(name_list)
+for var in range(len(name_list)):
+    print(f"Hola {name_list[var]}")
 
